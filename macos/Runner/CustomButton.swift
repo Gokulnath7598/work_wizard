4843e7f19957218d69e4c254a572f6df8bfadd3a
Yazhini Malar Pari <yazhinimalarpari@Yazhinis-MacBook-Air.local>
2024-06-15 15:38:10 +0530

    Loader inserted

--- macos/Runner/CustomButton.swift
+++ macos/Runner/CustomButton.swift
@@ -6,18 +6,25 @@ struct CustomButton: View {
     let isFilled: Bool?
     let isLoading: Bool?
     let onPressAction: () -> Void
+
     var body: some View {
-        Button(action: onPressAction){
-            Text(isLoading == true ? "Loading..." : label)
-                .foregroundColor(isFilled == true ? Color.white : buttonThemeColor)
-                .padding(.vertical, 13.5)
-                .frame(maxWidth: .infinity)
-        }.background(isFilled != true ? Color.white : buttonThemeColor)
-            .cornerRadius(8)
-            .overlay(
-                RoundedRectangle(cornerRadius: 8)
-                    .stroke(buttonThemeColor, lineWidth: 1.5)
-            )
+        Button(action: onPressAction) {
+            if isLoading == true {
+                LottieView(filename: "time_loader_50.json")
+                    .frame(maxWidth: .infinity, minHeight: 30)
+            } else {
+                Text(label)
+                    .foregroundColor(isFilled == true ? Color.white : buttonThemeColor)
+                    .padding(.vertical, 13.5)
+                    .frame(maxWidth: .infinity)
+            }
+        }
+        .background(isFilled != true ? Color.white : buttonThemeColor)
+        .cornerRadius(8)
+        .overlay(
+            RoundedRectangle(cornerRadius: 8)
+                .stroke(buttonThemeColor, lineWidth: 1.5)
+        )
     }
 }
 
