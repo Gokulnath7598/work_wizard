1b67215a2e9dd9078b6d6e964a3eb33246e2d6ee
Yazhini Malar Pari <yazhinimalarpari@Yazhinis-MacBook-Air.local>
2024-06-15 10:47:52 +0530

    Added Lottie Animation Overlay

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -1,80 +1,110 @@
 import Cocoa
 import FlutterMacOS
 import SwiftUI
+import Lottie
 
 @NSApplicationMain
 class AppDelegate: FlutterAppDelegate {
-  var overlayWindow: NSWindow?
+    var overlayWindow: NSWindow?
 
-  override func applicationDidFinishLaunching(_ aNotification: Notification) {
-    let flutterViewController = mainFlutterWindow?.contentViewController as! FlutterViewController
-    let channel = FlutterMethodChannel(name: "overlay_channel", binaryMessenger: flutterViewController.engine.binaryMessenger)
-    
-    channel.setMethodCallHandler { (call, result) in
-      if call.method == "showOverlay" {
-        self.showOverlay()
-        result(nil)
-      } else if call.method == "closeOverlay" {
-        self.closeOverlay()
-        result(nil)
-      } else {
-        result(FlutterMethodNotImplemented)
-      }
+    override func applicationDidFinishLaunching(_ aNotification: Notification) {
+        let flutterViewController = mainFlutterWindow?.contentViewController as! FlutterViewController
+        let channel = FlutterMethodChannel(name: "overlay_channel", binaryMessenger: flutterViewController.engine.binaryMessenger)
+        
+        channel.setMethodCallHandler { (call, result) in
+            if call.method == "showOverlay" {
+                self.showOverlay()
+                result(nil)
+            } else if call.method == "closeOverlay" {
+                self.closeOverlay()
+                result(nil)
+            } else {
+                result(FlutterMethodNotImplemented)
+            }
+        }
+
+        super.applicationDidFinishLaunching(aNotification)
     }
 
-    super.applicationDidFinishLaunching(aNotification)
-  }
-
-  func showOverlay() {
-    if overlayWindow == nil {
-      let overlayContentRect = NSRect(x: 100, y: 100, width: 200, height: 100)
-      overlayWindow = NSWindow(contentRect: overlayContentRect,
-                               styleMask: [.borderless],
-                               backing: .buffered, defer: false)
-      overlayWindow?.isOpaque = false
-      overlayWindow?.backgroundColor = NSColor.clear
-      overlayWindow?.level = .floating
-      overlayWindow?.ignoresMouseEvents = false
-      overlayWindow?.collectionBehavior = [.canJoinAllSpaces, .fullScreenAuxiliary]
-      
-      let contentView = NSHostingView(rootView: OverlayContentView(closeOverlay: closeOverlay))
-      overlayWindow?.contentView = contentView
+    func showOverlay() {
+        if overlayWindow == nil {
+            let overlayContentRect = NSRect(x: 100, y: 100, width: 200, height: 100)
+            overlayWindow = NSWindow(contentRect: overlayContentRect,
+                                     styleMask: [.borderless],
+                                     backing: .buffered, defer: false)
+            overlayWindow?.isOpaque = false
+            overlayWindow?.backgroundColor = NSColor.clear
+            overlayWindow?.level = .floating
+            overlayWindow?.ignoresMouseEvents = false
+            overlayWindow?.collectionBehavior = [.canJoinAllSpaces, .fullScreenAuxiliary]
+            
+            let contentView = NSHostingView(rootView: OverlayContentView(closeOverlay: closeOverlay))
+            overlayWindow?.contentView = contentView
+        }
+        overlayWindow?.makeKeyAndOrderFront(nil)
+        overlayWindow?.orderFrontRegardless()
     }
-    overlayWindow?.makeKeyAndOrderFront(nil)
-    overlayWindow?.orderFrontRegardless()
-  }
 
-  func closeOverlay() {
-    overlayWindow?.orderOut(nil)
-    overlayWindow = nil
-  }
+    func closeOverlay() {
+        overlayWindow?.orderOut(nil)
+        overlayWindow = nil
+    }
 }
 
 struct OverlayContentView: View {
-        let closeOverlay: () -> Void // Callback to close the overlay
+    let closeOverlay: () -> Void
 
-        init(closeOverlay: @escaping () -> Void) {
-            self.closeOverlay = closeOverlay
-        }
-  var body: some View {
-      HStack {
-                      Spacer()
-                      Button(action: {
-                          closeOverlay()
-                      }) {
-                          Image(systemName: "xmark.circle")
-                              .foregroundColor(.white)
-                              .font(.title)
-                      }
-                      .padding()
-                  }
-    VStack {
-      Text("Overlay Widget")
-        .foregroundColor(.white)
-        .frame(width: 200, height: 100)
-        .background(Color.blue)
-        .cornerRadius(10)
-        .padding()
+    init(closeOverlay: @escaping () -> Void) {
+        self.closeOverlay = closeOverlay
     }
-  }
+
+    var body: some View {
+        HStack {
+            Spacer()
+            Button(action: {
+                closeOverlay()
+            }) {
+                if #available(macOS 11.0, *) {
+                    Image(systemName: "xmark.circle")
+                        .foregroundColor(.white)
+                        .font(.title)
+                } else {
+                    
+                }
+            }
+            .padding()
+        }
+        VStack {
+            LottieView(filename: "cat_wizard_150.json")
+        }
+    }
 }
+
+    struct LottieView: NSViewRepresentable {
+        var filename: String
+
+        func makeNSView(context: Context) -> NSView {
+            let view = NSView()
+
+            let animationView = LottieAnimationView(name: filename)
+            animationView.contentMode = .scaleAspectFit
+            animationView.loopMode = .loop
+            animationView.play()
+
+            animationView.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview(animationView)
+
+            NSLayoutConstraint.activate([
+                animationView.widthAnchor.constraint(equalTo: view.widthAnchor),
+                animationView.heightAnchor.constraint(equalTo: view.heightAnchor)
+            ])
+
+            return view
+        }
+
+        func updateNSView(_ nsView: NSView, context: Context) {
+            
+        }
+    }
+
+
